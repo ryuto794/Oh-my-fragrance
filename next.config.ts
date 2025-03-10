c9af0d5e28118baa41f3ee2eab162565b2ff6285
Ryuto-Kawamata <81784756+Ryuto-Kawamata@users.noreply.github.com>
2025-03-10 12:57:08 +0900

    GitHub Pages用の設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
+// GitHub Pagesのリポジトリ名を取得
+const repo = 'Oh-my-fragrance';
+const assetPrefix = process.env.NODE_ENV === 'production' ? `/${repo}` : '';
+const basePath = process.env.NODE_ENV === 'production' ? `/${repo}` : '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // GitHub Pages用の設定
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+  // 出力をstaticにする
+  output: 'export',
 };
 
 export default nextConfig;
